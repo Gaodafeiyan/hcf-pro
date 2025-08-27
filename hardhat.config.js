0eb2e3e22de80f5b9d927d9370376bde886b3697
Gaodafeiyan <candicexfx18@gmail.com>
2025-08-28 01:49:40 +0800

    Add: BSC testnet network configuration to hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -10,6 +11,23 @@ module.exports = {
         enabled: true,
         runs: 200
       }
+    }
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    bscTestnet: {
+      url: process.env.BSC_TESTNET_RPC || "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      chainId: 97,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      gasPrice: 10000000000, // 10 gwei
+      timeout: 60000
+    }
+  },
+  etherscan: {
+    apiKey: {
+      bscTestnet: process.env.BSCSCAN_API_KEY || ""
     }
   }
 };
